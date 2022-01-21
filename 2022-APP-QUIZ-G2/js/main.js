5c7aef5f947d6517a995b56aaefac16ef50c6ac4
Kimky.MAO <Kimky.MAO@PNC.LAN>
2022-01-21 14:24:29 +0700

    #3 html

---
+++ 2022-APP-QUIZ-G2/js/main.js
@@ -0,0 +1,6 @@
+// Get button from form
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
